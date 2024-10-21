02c9140a4b1a65188c181291704229cd7f551983
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-10-21 22:42:47 +0200

    feat(seo): added meta

--- packages/app/src/entry-server.tsx
+++ packages/app/src/entry-server.tsx
@@ -9,13 +9,14 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
-          <title>PasFuteFute</title>
+          <title>PasFuteFute - Le répertoire des expressions françaises pour décrire les moins futé·e·s.</title>
           <meta name="description" content="Le répertoire des expressions françaises pour décrire les moins futé·e·s." />
           <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <meta name="apple-mobile-web-app-title" content="PasFuteFute" />
           <link rel="manifest" href="/site.webmanifest" />
+          <link rel="canonical" href="https://pasfutefute.fr/" />
           {assets}
         </head>
         <body>
