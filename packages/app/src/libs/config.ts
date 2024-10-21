06da9582d54cf37564048172483e4d64401f3e8f
Corentin Thomasset <corentin.thomasset74@gmail.com>
2024-10-21 13:08:52 +0200

    refactor(config): update base URL fallback logic

--- packages/app/src/libs/config.ts
+++ packages/app/src/libs/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   // eslint-disable-next-line node/prefer-global/process
-  baseUrl: import.meta.env.VITE_APP_BASE_URL ?? process?.env?.CF_PAGES_URL ?? window?.location?.origin ?? 'http://localhost:3000',
+  baseUrl: import.meta.env.VITE_APP_BASE_URL ?? process?.env?.CF_PAGES_URL ?? (import.meta.env.SSR ? 'http://localhost:3000' : window?.location?.origin),
 };
